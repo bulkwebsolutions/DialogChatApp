691b068c0e7fb9d08fe062d1752c402cf108a923
Alex Knows <cruzbiz11@gmail.com>
2018-05-22 15:58:12 -0400

    Added register form component

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from "@angular/core";
 import { IonicModule } from "ionic-angular";
 import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
 
 
 @NgModule({
    declarations: [
-     LoginFormComponent
+     LoginFormComponent,
+     RegisterFormComponent
    ],
    imports: [
      IonicModule
    ],
    exports: [
-     LoginFormComponent
+     LoginFormComponent,
+     RegisterFormComponent
    ]
 })
 
